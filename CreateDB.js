736a0d0a3cc360b55918f4c0aff14f82e608d141
KyzmenkoDiana <kyzmenko.d.1.21@gmail.com>
2022-11-14 14:53:02 +0300

    ПОДКЛЮЧЕНИЕ БАЗЫ ДАННЫХ

---
+++ CreateDB.js
@@ -0,0 +1,18 @@
+var MongoClient = require('mongodb').MongoClient
+
+const uri = "mongodb://localhost:27017/"
+const client = new MongoClient(uri)
+async function run() {
+    try {
+        await client.connect();
+        var database = client.db("ProjectAvocado");
+        database.dropDatabase()
+        database = client.db("ProjectAvocado");
+        const Avocado = database.collection("Avocado");
+        const result = await Avocado.insertOne({ name: "Флафи" });
+        console.log(`${result} documents were inserted`);
+    } finally {
+        await client.close();
+    }
+}
+run()
