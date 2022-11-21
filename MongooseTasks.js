28003bcb86fabe9dd48bb6eeab689b6fb1113c77
KyzmenkoDiana <kyzmenko.d.1.21@gmail.com>
2022-11-21 16:16:31 +0300

    СОЗДАНИЕ СХЕМЫ

--- MongooseTasks.js
+++ MongooseTasks.js
@@ -1,13 +1,15 @@
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/test')
 
-var Avocado = mongoose.model('Avocado', { name: String })
+var schema = mongoose.Schema({ name: String })
+
+schema.methods.cric = function () {
+    console.log(this.get("name") + " сказала пчхи")
+}
+
+var Avocado = mongoose.model('Avocado', schema)
 
 var kitty = new Avocado({ name: 'Gabbi' })
 kitty.save(function (err) {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log('пчхи')
-    }
+    kitty.cric()
 })
