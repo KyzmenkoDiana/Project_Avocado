f6dd80e9249cc77b96f2186429b55e00a448737d
KyzmenkoDiana <kyzmenko.d.1.21@gmail.com>
2022-11-21 16:45:19 +0300

    СОЗДАНИЕ МОДЕЛИ ДАННЫХ ПРИЛОЖЕНИЯ

--- MongooseTasks.js
+++ MongooseTasks.js
@@ -2,13 +2,11 @@ var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/test1')
 var Avocado = require("./models/Avocado.js").Avocado
 
-
 var Avocado = new Avocado({
     title: "Габби",
 })
 
 console.log(Avocado)
-Avocado.save(function () {
-    console.log(arguments)
+cat.save(function (err, cat, affected) {
+    console.log(cat.title)
 })
-
