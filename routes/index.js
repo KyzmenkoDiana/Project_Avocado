9d68086cc0efd54411b1fa0ebb327d045e72192a
KyzmenkoDiana <kyzmenko.d.1.21@gmail.com>
2022-12-02 18:58:11 +0300

    КОМАНДА ЗАПИСИ В COOKIE

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var Avocado = require("../models/Avocado").Avocado
 /* GET home page. */
 router.get('/', function (req, res, next) {
     Avocado.find({}, { _id: 0, title: 1, nick: 1 }, function (err, menu) {
-        res.render('index', {
+        res.cookie('greeting', 'Hi!!!').render('index', {
             title: 'Express',
             menu: menu
         });
