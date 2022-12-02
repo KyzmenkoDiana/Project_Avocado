53c4648fc2982e04dc683c12cdcaeedc67b09afa
KyzmenkoDiana <kyzmenko.d.1.21@gmail.com>
2022-12-02 21:47:45 +0300

    ЛОГИКА ПОЛЬЗОВАТЕЛЯ

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var router = express.Router()
 var Avocado = require("../models/Avocado").Avocado
+var User = require("./../models/user").User
 
 
 /* GET home page. */
@@ -23,8 +24,24 @@ router.get('/logreg', function (req, res, next) {
 router.post('/logreg', function (req, res, next) {
     var username = req.body.username
     var password = req.body.password
-
-
+    User.findOne({ username: username }, function (err, user) {
+        if (err) return next(err)
+        if (user) {
+            if (user.checkPassword(password)) {
+                req.session.user = user._id
+                res.redirect('/')
+            } else {
+                res.render('logreg', { title: 'Вход' })
+            }
+        } else {
+            var user = new User({ username: username, password: password })
+            user.save(function (err, user) {
+                if (err) return next(err)
+                req.session.user = user._id
+                res.redirect('/')
+            })
+        }
+    })
 });
 
 /*
