b87ed07b5e59563de1f9bf69c6eed86fd71d947a
KyzmenkoDiana <kyzmenko.d.1.21@gmail.com>
2022-12-02 19:15:42 +0300

    СОХРАНЕНИЕ SESSION В MONGODB

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,11 @@ var Avocado = require("../models/Avocado").Avocado
 /* GET home page. */
 router.get('/', function (req, res, next) {
     Avocado.find({}, { _id: 0, title: 1, nick: 1 }, function (err, menu) {
-        res.cookie('greeting', 'Hi!!!').render('index', {
-            title: 'Express',
-            menu: menu
-        });
+        req.session.greeting = "Hi!!!",
+            res.cookie('greeting', 'Hi!!!').render('index', {
+                title: 'Express',
+                menu: menu
+            });
     })
 
 });
